ea55e9e42d871f5aaa06daab783af4ac18d786ba
ry2009 <134240944+ry2009@users.noreply.github.com>
2024-03-18 19:24:56 -0400

    finished
    hi

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/.ipynb_checkpoints/consumer_interface-checkpoint.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/.ipynb_checkpoints/consumer_interface-checkpoint.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 
 
-class mqProducerInterface:
-    def __init__(self, routing_key: str, exchange_name: str) -> None:
+class mqConsumerInterface:
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str
+    ) -> None:
         # Save parameters to class variables
 
         # Call setupRMQConnection
@@ -25,15 +27,35 @@ class mqProducerInterface:
 
         # Establish Channel
 
+        # Create Queue if not already present
+
         # Create the exchange if not already present
+
+        # Bind Binding Key to Queue on the exchange
+
+        # Set-up Callback function for receiving messages
+        pass
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+        # Acknowledge message
+
+        #Print message (The message is contained in the body parameter variable)
 
         pass
 
-    def publishOrder(self, message: str) -> None:
-        # Basic Publish to Exchange
+    def startConsuming(self) -> None:
+        # Print " [*] Waiting for messages. To exit press CTRL+C"
 
+        # Start consuming messages
+        pass
+    
+    def __del__(self) -> None:
+        # Print "Closing RMQ connection on destruction"
+        
         # Close Channel
 
         # Close Connection
-    
+        
         pass
